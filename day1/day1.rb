e4d381d8b244c36cba64823c5b8f09e2a44d0d55
Edward Deam <edwardcrdeam@gmail.com>
2020-12-02 19:05:23 +1100

    remove util from day 1

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,6 +1,6 @@
 require_relative 'lib/solution'
-require_relative 'lib/input_util'
 
-input = InputUtil.read_to_i
+input = File.read('input.txt').split("\n").map(&:to_i)
+
 puts "Part one: #{Solution.solve_part_one(input)}"
 puts "Part two: #{Solution.solve_part_two(input)}"
--- day1/lib/input_util.rb
+++
@@ -1,5 +0,0 @@
-module InputUtil
-  def self.read_to_i
-    File.read('input.txt').split("\n").map(&:to_i)
-  end
-end
